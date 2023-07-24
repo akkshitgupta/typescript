3f761a053fda70d5981ca7239b658c28e17ad031
akkshitgupta <adamantaksh18@gmail.com>
2023-07-24 19:19:23 +0530

    class implementing interface

--- interface.ts
+++ interface.ts
@@ -1,11 +1,33 @@
 /*  instead use interface to define the type of the object so that it can be used in multiple places
  */
-interface Person {
+interface PersonInterface {
   name: string;
   age: number;
 }
 
-function greet(person: Person): string {
+class Person implements PersonInterface {
+  /* 
+
+    also interface can be implemented by a class
+
+    implements keyword is used to implement an interface in a class and this class must have all the properties and methods of the interface
+
+    the class object must build on top of the interface
+    */
+  name: string;
+  age: number;
+  address: string;
+
+  // constructor must initialise the properties of the class implementing the interface
+
+  constructor(name: string, age: number, address: string) {
+    this.name = name;
+    this.age = age;
+    this.address = address;
+  }
+}
+
+function greet(person: PersonInterface): string {
   return (
     "Hello, " +
     person.name +
